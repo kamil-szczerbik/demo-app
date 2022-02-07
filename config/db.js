de50d78bc2ee1680febe69f2ee7e8ee3225a4587
Kamil Szczerbik <kamilsz176176@gmail.com>
2022-02-07 18:31:06 +0100

    ssl true sequelize

--- config/db.js
+++ config/db.js
@@ -1,6 +1,23 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}`);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
+);
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 const db = {};
 const model = require('../models/user.model')(sequelize, Sequelize.DataTypes);
